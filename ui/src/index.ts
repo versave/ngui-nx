6362bf39922b8e78f5211506ef89ef36ef68f71a
Vasil Zhivkov <vszhivkov@gmail.com>
2023-03-07 22:20:36 +0200

    Fix export

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,1 +1,6 @@
 export * from './lib/ui.module';
+export * from './lib/components/button/button.component';
+export * from './lib/components/checkbox/checkbox.component';
+export * from './lib/components/input-field/input-field.component';
+export * from './lib/components/radio-group/radio-group.component';
+export * from './lib/components/select/select.component';
